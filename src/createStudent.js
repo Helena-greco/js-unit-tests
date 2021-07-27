7769acfddd413f218ba3b5b0178fbbc446cca402
Helena <nenafgreco@hotmail.com>
2021-07-27 18:24:19 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const estudante = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!', // return da function está implícito.
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
